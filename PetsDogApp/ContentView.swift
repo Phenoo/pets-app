434a9123a8029c08d86b7762214f1c27654fa5a6
Phenoo <descometusah@gmail.com>
2024-09-19 02:33:14 +0100

    Initial Commit

---
+++ PetsDogApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetsDogApp
+//
+//  Created by Eze Chidera Paschal on 19/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetsDogApp/PetsDogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsDogAppApp.swift
+//  PetsDogApp
+//
+//  Created by Eze Chidera Paschal on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PetsDogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
